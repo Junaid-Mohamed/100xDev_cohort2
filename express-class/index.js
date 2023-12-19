02a5e5d0ea74da168689b683a5e54544da6a5900
Junaid-Mohamed <junaid7mohamed@gmail.com>
2023-12-19 23:08:31 +0530

    created CRUD routes for hospital manag System

--- express-class/index.js
+++ express-class/index.js
@@ -1,3 +1,68 @@
 const express = require("express");
-
 const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+  {
+    name: "Johny",
+    kidneys: [
+      {
+        healthy: false,
+      },
+      {
+        healthy: true,
+      },
+    ],
+  },
+];
+
+app.get("/", (req, res) => {
+  const johnKidneys = users[0].kidneys;
+  const noOfKidneys = johnKidneys.length;
+  let noOfHealthyKidneys = johnKidneys.filter(
+    (kid) => kid.healthy === true
+  ).length;
+  //   console.log(noOfHealthyKidneys);
+  let noOfUnhealthyKidneys = noOfKidneys - noOfHealthyKidneys;
+  res.json({
+    noOfKidneys,
+    noOfHealthyKidneys,
+    noOfUnhealthyKidneys,
+  });
+});
+
+app.post("/", (req, res) => {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    users,
+  });
+});
+
+app.put("/", (req, res) => {
+  users.map((user) => {
+    user.kidneys.map((kid) => {
+      kid.healthy = true;
+    });
+  });
+  res.json({});
+});
+
+app.delete("/", (req, res) => {
+  const newKidneys = [];
+  users.map((user) => {
+    user.kidneys.filter((kid) => {
+      if (kid.healthy) newKidneys.push(kid);
+    });
+    user.kidneys = newKidneys;
+  });
+  //   let newKidneys = johnKidneys.filter((kid) => kid.healthy === true);
+  //   console.log(newKidneys);
+  res.json({ msg: "done" });
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
