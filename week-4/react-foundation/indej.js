8576e2b0580710e700b83acf1c758099fef9e5a2
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-03 12:55:18 +0530

    added adtodoToDom functionlity

--- week-4/react-foundation/indej.js
+++ week-4/react-foundation/indej.js
@@ -2,29 +2,21 @@ const addtodoButton = document.getElementById("addToDo");
 
 let todoId = 1;
 let todo = [];
-let oldTodo = [];
+let oldTodoState = [];
 
 function markAsDone(id) {
   const parent = document.getElementById(id);
   parent.children[2].innerHTML = "Done";
 }
 
-function deleteTodo(id) {
+function deleteToDo(id) {
   const todo = document.getElementById(id);
   console.log(todo);
 }
 
-function updateTodo(id) {
+function updateToDo(id) {
   const todo = document.getElementById(id);
   console.log(todo);
-}
-
-function isEqual(todo1, todo2) {
-  const bool =
-    todo1.id === todo2.id &&
-    todo1.title === todo2.title &&
-    todo1.desc === todo2.desc;
-  return bool;
 }
 
 // https://sum-server.100xdevs.com/todos
@@ -61,8 +53,6 @@ function addToDo() {
     desc: desc,
     id: todoId,
   });
-  // console.log(todo);
-
   updateState(todo);
 }
 
@@ -70,55 +60,35 @@ function updateState(todos) {
   // console.log(todos);
 
   const added = [];
-  const deleted = [];
-  const updated = [];
-
+  // const deleted = [];
+  // const updated = [];
+  // console.log("oldtodo", oldTodoState);
   // find added todo's
-  todos.forEach((todo) => {
-    const existInOldTodo = oldTodo.some((oldTodo) => oldTodo.id === todo.id);
-    if (!existInOldTodo) {
-      added.push(todo);
-    }
-  });
-  // console.log(added);
 
-  // find deleted to do
-  oldTodo.forEach((oldTodo) => {
-    const existIntodo = todo.some((todo) => todo.id === oldTodo.id);
-    if (!existIntodo) {
-      deleted.push(todo);
-    }
+  const addedItems = todos.filter((item) => {
+    const bool = !oldTodoState.some(
+      (oldItem) =>
+        oldItem.id === item.id &&
+        oldItem.title === item.title &&
+        oldItem.desc === item.desc
+    );
+
+    return bool;
   });
 
-  // find updated todo
-  todos.forEach((todo) => {
-    const oldtodo = oldTodo.find((oldtodo) => oldtodo.id === todo.id);
-    if (oldtodo && !isEqual(oldtodo, todo)) {
-      updated.push(todo);
-    }
-  });
-
-  // call appropriate functions for added,deleted and updated.
-  added.forEach(addTodoToDom);
-  deleted.forEach(deleteTodoFromDom);
-  updated.forEach(updateTodoInDom);
-
-  // update the old state
-  oldTodo = todo;
+  if (addedItems.length > 0) {
+    added.push(addedItems[0]);
+    oldTodoState = [...todo];
+    console.log("oldTodoState", oldTodoState);
+  }
+  addTodoToDom(added);
 }
 
 function addTodoToDom(todo) {
+  // console.log(todo[0].title);
   const parent = document.getElementById("container");
-  parent.appendChild(createChild(todo.title, todo.desc, todoId++));
+  parent.appendChild(createChild(todo[0].title, todo[0].desc, todoId++));
   // console.log(todo);
-}
-
-function deleteTodoFromDom(todo) {
-  console.log(todo);
-}
-
-function updateTodoInDom(todo) {
-  console.log(todo);
 }
 
 addtodoButton.addEventListener("click", addToDo);
