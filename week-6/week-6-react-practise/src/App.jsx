178d684ee7dd3dbcd87fc4b6d6206bac29b924b4
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-13 15:12:24 +0530

    hooks understanding code

--- week-6/week-6-react-practise/src/App.jsx
+++ week-6/week-6-react-practise/src/App.jsx
@@ -1,28 +1,45 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputvalue] = useState(1);
+  const [sum, setSum] = useState(0);
+  useEffect(() => {
+    let count = 0;
+    for (let i = 0; i <= inputValue; i++) {
+      count += i;
+    }
+  }, [inputValue]);
+
+  function handleClick() {
+    setCounter(counter + 1);
+  }
+
+  function handleChange(e) {
+    setInputvalue(e.target.value);
+  }
+
   return (
-    <div>
-      <CardComponent innerComponent={<TextComponent />} />;
+    <div style={{ margin: "8px" }}>
+      <input style={{ margin: "8px" }} onChange={handleChange} type="number" />
+      <br />
+      Sum from 1 to {inputValue} is {}
+      <br />
+      <button style={{ margin: "8px" }} onClick={handleClick}>
+        Count {counter}
+      </button>
     </div>
   );
 }
 
-function TextComponent() {
-  return <div>Hi There!</div>;
-}
+function Todo({ id }) {
+  // do a network call.
 
-function CardComponent({ innerComponent }) {
-  // console.log();
-  return (
-    <div style={{ border: "2px solid black", padding: "10px" }}>
-      {innerComponent}
-    </div>
-  );
+  return <div>this is todo with id {id}</div>;
 }
 
 export default App;
