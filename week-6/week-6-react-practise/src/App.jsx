5a0c5e857c027aa0705f54ca196fb947aa12722b
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-14 15:36:10 +0530

    memo hook learnings

--- week-6/week-6-react-practise/src/App.jsx
+++ week-6/week-6-react-practise/src/App.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { Fragment, memo, useEffect, useMemo, useState } from "react";
 import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -13,37 +13,38 @@ function App() {
   useEffect(() => {
     setExchange1Data({ returns: 100 });
   }, []);
-  useEffect(() => {
-    setTimeout(() => {
-      setBankData({ income: 100 });
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setBankData({ income: 100 });
+  //   }, 3000);
+  // }, []);
   useEffect(() => {
     setExchang2eData({ returns: 100 });
   }, []);
 
-  function handleClick() {
-    setCounter((counter) => counter + 1);
-    setCounter((counter) => counter + 1);
-  }
-
-  function handleChange(e) {
-    setInputvalue(e.target.value);
-  }
-
-  // learning the concept of useMemo,
-  // after the bankdata is called rerender happens but cryptoreturn are already
-  // calculated, so it should not be called again.
-  // useEffect and useMemo if you look it is same, execution of particular codes happens when a dependency array changes
-  // but useEffect doesn't return anything that's the difference
-  const cryptoReturns = useMemo(() => {
-    console.log("crypto return called");
+  // useCallback is not about minimising the amount of code that is run, but useMemo is.
+  // useCallback is not about rendering a child component, if the function hasn't/doesn't need to change across renders
+  const calculateCryptoReturns = () => {
     return exchange1Data.returns + exchange2Data.returns;
-  }, [exchange1Data, exchange2Data]);
-  console.log("crypto returns", cryptoReturns);
-  const incomeTax = (bankData.income + cryptoReturns) * 0.3;
-
-  return <div>Hi There, your income tax return are {incomeTax}</div>;
+  };
+  return (
+    <div>
+      <CryptoReturnsCalculator
+        calculateCryptoReturns={calculateCryptoReturns}
+      />
+      <DummyComponent />
+    </div>
+  );
 }
+
+const CryptoReturnsCalculator = memo(({ calculateCryptoReturns }) => {
+  return <div>Your crypto return are {calculateCryptoReturns()}</div>;
+});
+
+// memo lets the component to render only the props passed to it changes.
+const DummyComponent = memo(() => {
+  console.log("child component rendered");
+  return <div>Hi There</div>;
+});
 
 export default App;
