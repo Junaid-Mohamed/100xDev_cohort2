19634eb0bbca2b2dc7480a514ce8f5f7431635dd
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-14 15:03:03 +0530

    useMemo hook learnings

--- week-6/week-6-react-practise/src/App.jsx
+++ week-6/week-6-react-practise/src/App.jsx
@@ -1,23 +1,25 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 import React from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [exchangeData, setExchangeData] = useState({});
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchang2eData] = useState({});
   const [bankData, setBankData] = useState({});
 
   console.log("rerender happened");
+  useEffect(() => {
+    setExchange1Data({ returns: 100 });
+  }, []);
   useEffect(() => {
     setTimeout(() => {
       setBankData({ income: 100 });
     }, 3000);
   }, []);
   useEffect(() => {
-    setTimeout(() => {
-      setExchangeData({ returns: 100 });
-    }, 2000);
+    setExchang2eData({ returns: 100 });
   }, []);
 
   function handleClick() {
@@ -29,7 +31,17 @@ function App() {
     setInputvalue(e.target.value);
   }
 
-  const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
+  // learning the concept of useMemo,
+  // after the bankdata is called rerender happens but cryptoreturn are already
+  // calculated, so it should not be called again.
+  // useEffect and useMemo if you look it is same, execution of particular codes happens when a dependency array changes
+  // but useEffect doesn't return anything that's the difference
+  const cryptoReturns = useMemo(() => {
+    console.log("crypto return called");
+    return exchange1Data.returns + exchange2Data.returns;
+  }, [exchange1Data, exchange2Data]);
+  console.log("crypto returns", cryptoReturns);
+  const incomeTax = (bankData.income + cryptoReturns) * 0.3;
 
   return <div>Hi There, your income tax return are {incomeTax}</div>;
 }
