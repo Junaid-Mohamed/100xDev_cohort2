5ea75d5c6590f4b5a69f1ce39f645f812d5ca2a7
Junaid-Mohamed <junaid7mohamed@gmail.com>
2024-01-14 19:34:18 +0530

    routing using useNavigate hook

--- week7/react-routing/src/App.jsx
+++ week7/react-routing/src/App.jsx
@@ -2,36 +2,43 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard";
 import { Landing } from "./components/Landing";
 
 function App() {
   return (
     <div>
-      <div>
-        <button
-          onClick={() => {
-            window.location.href = "/";
-          }}
-        >
-          {" "}
-          Landing
-        </button>
-        <button
-          onClick={() => {
-            window.location.href = "/dashboard";
-          }}
-        >
-          Dahsboard
-        </button>
-      </div>
       <BrowserRouter>
+        <AppBar />
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Landing />} />
         </Routes>
       </BrowserRouter>
+    </div>
+  );
+}
+
+function AppBar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        {" "}
+        Landing
+      </button>
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        Dahsboard
+      </button>
     </div>
   );
 }
