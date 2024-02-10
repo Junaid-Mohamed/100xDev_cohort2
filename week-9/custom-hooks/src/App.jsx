12c057faa3e9cf81c4eb2c08f3e6e241ab15dcf0
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-02-10 11:31:24 +0530

    normal todo rendering

--- week-9/custom-hooks/src/App.jsx
+++ week-9/custom-hooks/src/App.jsx
@@ -2,41 +2,59 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [render,setRender] = useState(true);
+  
+  const [todos,setTodos] = useState([]);
 
   useEffect(()=>{
-    setTimeout(()=>{
-      setRender((render)=>!render)
-    },3000)
-  })
+      axios.get("https://sum-server.100xdevs.com/todos")
+      .then(resp=>setTodos(resp.data.todos));
+
+  },[])
   return (
     <>
-    <Mycomponent/>
+   { todos.map((todo)=>(<Todos title={todo.title} desc={todo.description} />))}
+    
     </>
   )
 }
 
-const Mycomponent = () =>{
-  
-  const [count,setCount] = useState(0);
-
-  useEffect(()=>{
-    console.log("component mounted");
-
-    return ()=>{
-      console.log("component unmounted")
-    }
-  },[count]);
+const Todos = ({title,desc}) =>{
 
   return(
     <div>
-      <h3>{count}</h3>
-      <button onClick={()=>setCount((count)=>count+1)}>Increment</button>
-      <button onClick={()=>setCount((count)=>count-1)}>Decrement</button>
+      <h3>{title}</h3>
+      <p>{desc}</p>
     </div>
   )
+
 }
 
+
+
 export default App
+
+
+
+// const Mycomponent = () =>{
+  
+//   const [count,setCount] = useState(0);
+
+//   useEffect(()=>{
+//     console.log("component mounted");
+
+//     return ()=>{
+//       console.log("component unmounted")
+//     }
+//   },[count]);
+
+//   return(
+//     <div>
+//       <h3>{count}</h3>
+//       <button onClick={()=>setCount((count)=>count+1)}>Increment</button>
+//       <button onClick={()=>setCount((count)=>count-1)}>Decrement</button>
+//     </div>
+//   )
+// }
