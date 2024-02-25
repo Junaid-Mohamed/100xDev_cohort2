83d901b61e88dc2795a3befc67f3149e52745535
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-02-25 15:02:03 +0530

    added interface structure

--- week-9/typescript/node-app/sample.ts
+++ week-9/typescript/node-app/sample.ts
@@ -1,26 +1,30 @@
-const x:number = 10;
-console.log(x)
+// on way for defining type for object.
+// but when you have to use this obj at differenet places it would require you to re write this type again in that place.
+// which voilates the DRY principle,
+// hence you use the interface here
 
-function greet(name:String){
-    console.log("Hi ",name);
-}
-
-// ?if you return a+b then ts will be able to type infer that return is of type number only,
-// but what if they return string or any other type hence writing the return type is imp.
-function sum(a:number,b:number):number{
-    console.log("Sum=",a+b);
-    return a+b;
-}
-
-function isLegal(age:number):Boolean{
-    if(age>=18) return true;
+function isLegal(user:{
+    firstName:string,
+    lastName:string,
+    age:number
+}):Boolean{
+    if(user.age>=18) return true;
     else return false;
 }
 
-function takeInputFunction(cb:){
-    setTimeout(()=>{
-        cb();
-    },1000)
+function isLegal1(user:User):Boolean{
+    if(user.age>=18) return true;
+    else return false;
 }
 
-takeInputFunction(()=>{console.log("Hi Worlds")})
+interface User{
+        firstName:string,
+        lastName:string,
+        age:number
+}
+
+isLegal({
+    firstName:"Mohammed",
+    lastName:"Junaid",
+    age:25
+})
